c57d000e55ff6cb6374c169ac43fd8dab6f5db59
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-09-04 19:57:05 +0700

    Add generateRandColor when hovering grid

--- app.js
+++ app.js
@@ -10,5 +10,16 @@ for (let i = 0; i < 256; i++) {
 const grids = document.querySelectorAll(".grid");
 
 for (let grid of grids) {
-  grid.addEventListener("click", () => console.log("clicked"));
+  grid.addEventListener(
+    "mouseenter",
+    () => (grid.style.backgroundColor = generateRandColors()),
+  );
+}
+
+function generateRandColors() {
+  const R = Math.floor(Math.random() * 256);
+  const G = Math.floor(Math.random() * 256);
+  const B = Math.floor(Math.random() * 256);
+
+  return `rgb(${R}, ${G}, ${B})`;
 }
