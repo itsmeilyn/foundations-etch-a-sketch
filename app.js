9632470dcc7ff8520271a670397a31534da1a2bd
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-09-04 19:39:03 +0700

    Add initial HTML, CSS, and JavaScript for Etch-a-Sketch project

---
+++ app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+
+// create 16x16 divs
+for (let i = 0; i < 255; i++) {
+  const div = document.createElement("div");
+  div.classList.add("grid");
+  container.appendChild(div);
+}
