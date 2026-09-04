60f7aaa2ef3e198fab0a388321a92da8aeb3be88
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-09-04 19:50:00 +0700

    Add grids inside container div

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const container = document.querySelector(".container");
 
 // create 16x16 divs
-for (let i = 0; i < 255; i++) {
+for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   div.classList.add("grid");
   container.appendChild(div);
+}
+
+const grids = document.querySelectorAll(".grid");
+
+for (let grid of grids) {
+  grid.addEventListener("click", () => console.log("clicked"));
 }
